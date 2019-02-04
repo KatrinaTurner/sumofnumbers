25d168c405c4227e90726f4da6b5f68db9285806
Katrina H. Turner <khj@hawaii.edu>
2019-02-03 17:54:31 -1000

    pau

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -9,19 +9,24 @@ function sumFor(list) {
 function sumWhile(list) {
   let sum = 0;
   let i = 0;
-  while(i < list.length) {
+  while (i < list.length) {
     sum += list[i];
     i++;
   }
   return sum;
 }
 function sumRecursion(list) {
-  if (list.length == 1) {
-    return list[0]
-  } else {
-    return list[0] + sumRecursion(list.subarray(1, list.length - 1));
+  if (list.length === 1) {
+    return list[0];
   }
+  return list[0] + sumRecursion(list.slice(1));
 }
 function sumTheSimpleWay(list) {
-  return _.reduce(list, function (num,memo) {return memo + sum},0);
+  return _.reduce(list, function (num, memo) { return memo + num; }, 0);
 }
+
+const testList = [1, 2, 3, 4];
+console.log(sumFor(testList));
+console.log(sumWhile(testList));
+console.log(sumRecursion(testList));
+console.log(sumTheSimpleWay(testList));
